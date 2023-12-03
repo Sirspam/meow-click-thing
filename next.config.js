18cc581cc1f7ac46530e9d702d0856d2b1b6dead
Sirspam <71392316+Sirspam@users.noreply.github.com>
2023-12-03 00:54:32 +0000

    Moved env logic to page.tsx

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.REPO_PATH,
-    assetPrefix: process.env.REPO_PATH,
     output: 'export'
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,39 @@
 "use client"
 
-import NyaSoundImage from "@/app/components/ui/NyaSoundImage";
-import {useTheme} from "@/app/components/ThemeProvider";
-import {useEffect} from "react";
+import NyaSoundImage from "@/app/components/ui/NyaSoundImage"
+import { useTheme } from "@/app/components/ThemeProvider"
+import { useEffect } from "react"
 
 interface NyaSoundImageValues {
-    imgPath: string;
-    imgAlt: string;
-    mp3Path: string;
+  imgPath: string
+  imgAlt: string
+  mp3Path: string
 }
 
 export default function Home() {
-    const {theme, setTheme} = useTheme()
+  const { theme, setTheme } = useTheme()
 
-    // Preload both images so they both load immediately when the theme is toggled
-    useEffect(() => {
-        new Image().src = "/chocola_mini_sitting.png"
-        new Image().src = "/vanilla_mini_sitting.png"
-    }, [])
+  // Preload both images so they both load immediately when the theme is toggled
+  useEffect(() => {
+    new Image().src = `${process.env.REPO_PATH || ""}/chocola_mini_sitting.png`
+    new Image().src = `${process.env.REPO_PATH || ""}/vanilla_mini_sitting.png`
+  }, [])
 
-    return (
+  return (
     <main className="flex overflow-hidden h-full items-center justify-center p-10 lg:p-0 bg-cyan-50 dark:bg-night-rider-950">
-        {theme === "dark" ?
-            <NyaSoundImage imgPath='/chocola_mini_sitting.png' imgAlt='Chocola' mp3Path='/chocola_meow.mp3'/> :
-            <NyaSoundImage imgPath='/vanilla_mini_sitting.png' imgAlt='Vanilla' mp3Path='/vanilla_meow.mp3'/>
-        }
+      {theme === "dark" ? (
+        <NyaSoundImage
+          imgPath={`${process.env.REPO_PATH || ""}/chocola_mini_sitting.png`}
+          imgAlt="Chocola"
+          mp3Path={`${process.env.REPO_PATH || ""}/chocola_meow.mp3`}
+        />
+      ) : (
+        <NyaSoundImage
+          imgPath={`${process.env.REPO_PATH || ""}/vanilla_mini_sitting.png`}
+          imgAlt="Vanilla"
+          mp3Path={`${process.env.REPO_PATH || ""}/vanilla_meow.mp3`}
+        />
+      )}
     </main>
   )
 }
