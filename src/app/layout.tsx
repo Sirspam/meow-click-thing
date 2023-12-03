3d8112326a73471c65d33ca1222b6e89eeba1d27
Sirspam <71392316+Sirspam@users.noreply.github.com>
2023-12-03 01:26:16 +0000

    Added metadata image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,37 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import ThemeProvider from "@/app/components/ThemeProvider";
-import SiteHeader from "@/app/components/ui/SiteHeader";
-import SiteFooter from './components/ui/SiteFooter';
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import ThemeProvider from "@/app/components/ThemeProvider"
+import SiteHeader from "@/app/components/ui/SiteHeader"
+import SiteFooter from "./components/ui/SiteFooter"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'meow click thing',
-  description: 'click the cat',
+  title: "meow click thing",
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_REPO_PATH ||
+      "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  ),
+  openGraph: {
+    images: ["/chocovani_cpuhappy.webp"],
+  },
 }
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
-    return (
-        <html lang="en">
-            <body className={`${inter.className} h-screen`}>
-                <ThemeProvider>
-                    <SiteHeader/>
-                    {children}
-                    <SiteFooter/>
-                </ThemeProvider>
-            </body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <body className={`${inter.className} h-screen`}>
+        <ThemeProvider>
+          <SiteHeader />
+          {children}
+          <SiteFooter />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
 }
