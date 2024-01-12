e8c78f07d9a482b82a496c5fe1ddb53ad8b7884e
Sirspam <71392316+Sirspam@users.noreply.github.com>
2024-01-12 23:46:32 +0000

    Limit header and footer content width

--- src/app/components/ui/NyaSoundImage.tsx
+++ src/app/components/ui/NyaSoundImage.tsx
@@ -1,18 +1,23 @@
 "use client"
 
-import useSound from "use-sound";
+import useSound from "use-sound"
 
 interface Props {
-    imgPath: string;
-    imgAlt: string;
-    mp3Path: string;
+  imgPath: string
+  imgAlt: string
+  mp3Path: string
 }
 
 export default function NyaSoundImage(props: Props) {
-    const [play] = useSound(props.mp3Path, { volume: 0.1 });
+  const [play] = useSound(props.mp3Path, { volume: 0.3 })
 
-    return (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img className="max-w-full max-h-full sm:max-w-[500px] flex aspect-auto" src={props.imgPath} alt={props.imgAlt} onClick={() => play()}/>
-    )
+  return (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      className="max-w-full max-h-full sm:max-w-[500px] flex aspect-auto"
+      src={props.imgPath}
+      alt={props.imgAlt}
+      onClick={() => play()}
+    />
+  )
 }
--- src/app/components/ui/SiteHeader.tsx
+++ src/app/components/ui/SiteHeader.tsx
@@ -1,10 +1,14 @@
-import ThemeToggle from "@/app/components/ui/ThemeToggle";
+import ThemeToggle from "@/app/components/ui/ThemeToggle"
 
 export default function SiteHeader() {
-    return (
-        <header className="h-16 px-3 flex items-center justify-between border-b-2 border-cyan-300 dark:border-night-rider-500 bg-cyan-200 dark:bg-night-rider-900">
-            <h1 className="font-extrabold tracking-wide text-2xl text-cyan-900 dark:text-night-rider-200">meow click thing</h1>
-            <ThemeToggle/>
-        </header>
-    )
+  return (
+    <header className="h-16 px-3 flex border-b-2 border-cyan-300 dark:border-night-rider-500 bg-cyan-200 dark:bg-night-rider-900">
+      <div className="container flex max-w-screen-2xl justify-between items-center">
+        <h1 className="font-extrabold tracking-wide text-2xl text-cyan-900 dark:text-night-rider-200">
+          meow click thing
+        </h1>
+        <ThemeToggle />
+      </div>
+    </header>
+  )
 }
