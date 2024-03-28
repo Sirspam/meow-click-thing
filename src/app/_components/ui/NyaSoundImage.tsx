228f9c4a418ae15cf2ff8d0d6d930b396f5456a9
Sirspam <71392316+Sirspam@users.noreply.github.com>
2024-03-28 01:01:44 +0000

    Image scale on click

--- src/app/_components/ui/NyaSoundImage.tsx
+++ src/app/_components/ui/NyaSoundImage.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState, useEffect } from "react"
 import useSound from "use-sound"
 
 interface Props {
@@ -9,15 +10,34 @@ interface Props {
 }
 
 export default function NyaSoundImage(props: Props) {
+  const defaultImgClass =
+    "max-w-full max-h-full sm:max-w-[500px] flex aspect-auto"
+  const [imgClassState, setImgClassState] = useState(defaultImgClass)
+
   const [play] = useSound(props.mp3Path, { volume: 0.5 })
+
+  useEffect(() => {
+    const handleMouseUp = () => {
+      if (imgClassState !== defaultImgClass) {
+        setImgClassState(defaultImgClass)
+      }
+    }
+    window.addEventListener("mouseup", handleMouseUp)
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener("mouseup", handleMouseUp)
+    }
+  }, [defaultImgClass, imgClassState])
 
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
-      className="max-w-full max-h-full sm:max-w-[500px] flex aspect-auto"
+      className={imgClassState}
       src={props.imgPath}
       alt={props.imgAlt}
       onClick={() => play()}
+      onMouseDown={() => setImgClassState(defaultImgClass + " scale-95")}
     />
   )
 }
