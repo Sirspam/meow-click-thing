7fc0ff7b31c511d933385cba511d51d82cefe6f4
Sirspam <71392316+Sirspam@users.noreply.github.com>
2024-03-28 00:27:40 +0000

    Improved code maintability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,46 +4,35 @@ import NyaSoundImage from "@/app/_components/ui/NyaSoundImage"
 import { useTheme } from "@/app/_components/ThemeProvider"
 import { useEffect } from "react"
 
-interface NyaSoundImageValues {
-  imgPath: string
-  imgAlt: string
-  mp3Path: string
-}
-
 export default function Home() {
+  // This is needed for Github Actions so that it can get the correct full path for resources
+  // A .env.local is needed for local development, which overrides the .env in the repo
+  // There surely must be a better way to do go about this, but I'm not sure how to do it
+  function ReturnLocalOrProdPath(path: string) {
+    return process.env.NEXT_PUBLIC_REPO_PATH || "" + "/" + path
+  }
+
   const { theme, setTheme } = useTheme()
 
   // Preload both images so they both load immediately when the theme is toggled
   useEffect(() => {
-    new Image().src = `${
-      process.env.NEXT_PUBLIC_REPO_PATH || ""
-    }/chocola_mini_sitting.webp`
-    new Image().src = `${
-      process.env.NEXT_PUBLIC_REPO_PATH || ""
-    }/vanilla_mini_sitting.webp`
+    new Image().src = ReturnLocalOrProdPath("chocola_mini_sitting.webp")
+    new Image().src = ReturnLocalOrProdPath("vanilla_mini_sitting.webp")
   }, [])
 
   return (
     <main className="flex overflow-hidden h-full items-center justify-center p-10 lg:p-0 bg-cyan-50 dark:bg-night-rider-950">
       {theme === "dark" ? (
         <NyaSoundImage
-          imgPath={`${
-            process.env.NEXT_PUBLIC_REPO_PATH || ""
-          }/chocola_mini_sitting.webp`}
+          imgPath={ReturnLocalOrProdPath("chocola_mini_sitting.webp")}
           imgAlt="Chocola"
-          mp3Path={`${
-            process.env.NEXT_PUBLIC_REPO_PATH || ""
-          }/chocola_meow.mp3`}
+          mp3Path={ReturnLocalOrProdPath("chocola_meow.mp3")}
         />
       ) : (
         <NyaSoundImage
-          imgPath={`${
-            process.env.NEXT_PUBLIC_REPO_PATH || ""
-          }/vanilla_mini_sitting.webp`}
+          imgPath={ReturnLocalOrProdPath("vanilla_mini_sitting.webp")}
           imgAlt="Vanilla"
-          mp3Path={`${
-            process.env.NEXT_PUBLIC_REPO_PATH || ""
-          }/vanilla_meow.mp3`}
+          mp3Path={ReturnLocalOrProdPath("vanilla_meow.mp3")}
         />
       )}
     </main>
